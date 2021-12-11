8d1cc4f499e8ffb70ecf7853306928168c0d90cc
Linas <l.kalusevicius@gmail.com>
2021-12-11 13:04:09 +0200

    Completed module

--- src/Quiz.js
+++ src/Quiz.js
@@ -8,6 +8,7 @@ class Quiz extends Component {
         this.state = { quiz_position: 1 }
     }
     render() {
+        return
         <div>
             <div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>
         </div>
