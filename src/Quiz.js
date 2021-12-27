06328f76e11181670a896b2b1092e76c4e449130
Linas <l.kalusevicius@gmail.com>
2021-12-27 20:38:36 +0200

    QuizQuestion component progress

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,9 +9,8 @@ class Quiz extends Component {
     }
     render() {
         return (
-            <div>
-                <div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>
-            </div>)
+            <QuizQuestion />
+        )
     }
 }
 
---
+++ src/QuizQuestion.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import QuizQuestion from QuizQuestion.js
+
+class Quiz extends Component {
+    render() {
+        return (
+            <main>
+                <section>
+                    <p>// instruction text goes here</p>
+                </section>
+                <section className="buttons">
+                    <ul>
+                    //quiz question button logic goes here
+                    </ul>
+                </section>
+            </main>
+        )
+    }
+}
+
+export default QuizQuestion
